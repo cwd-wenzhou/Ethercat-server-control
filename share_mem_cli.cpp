034cbf58693dfa600a2fd1d893ba1cf003a5c6a4
cwd-wenzhou <619715109@qq.com>
2021-05-16 15:54:25 +0800

    增加注释

--- share_mem_cli.cpp
+++ share_mem_cli.cpp
@@ -5,7 +5,15 @@
  * 基于共享内存，作为应用层，读共享内存里的ethercat过程数据，然后基于业务逻辑，操作数据并写入。
  * 现在业务逻辑只实现了position一种，其他尚待完善。
  * 调用方法：
- *      ./share_men_cli  2000
+ *      ./share_men_cli  <command> <arg>
+ * -h 回零
+ * -p <arg> 位置模式，运动到arg处
+ * -v <arg> 速度模式，以arg(rad/s)速度运行
+ * --todo
+ * 
+ * 例如
+ *  ./share_men_cli -h 0//表示命令电机回零
+ *  ./share_men_cli -p 1000  //表示命令电机运动到位置 1000
  *  其中2000表示期望运动到的位置。
  */
 #include <iostream>
@@ -15,6 +23,7 @@
 #include <unistd.h>
 #include "shm_ect.h"
 #include "header.h"
+#include "server.h"
 using namespace std;
 int main(int argc, char const *argv[])
 {
@@ -22,6 +31,7 @@ int main(int argc, char const *argv[])
     int shmfd = shm_open(SHM_NAME, O_RDWR  ,SHM_MODE);
     motor = (struct MOTOR* )mmap(NULL,sizeof(struct MOTOR),PROT_READ | PROT_WRITE,MAP_SHARED,shmfd,0);
     close(shmfd);
+
     //printf("that't all\n");
     if (argc<2){
         printf("put in position(0~%d)\n",ENCODER_RESOLUTION);
@@ -30,11 +40,23 @@ int main(int argc, char const *argv[])
     int position_=atoi(argv[1]);
     printf("*It's working now*\n");
 
-
+    char* cmd;
+    int arg;
     while (true){
         usleep(1000);
-        if (!Is_Serevr_On)                     
+        if (!Is_Serevr_On) 
             continue;
+        cin>>cmd>>arg;
+        switch (cmd)
+        {
+        case "-h":
+            /* code */
+            break;
+        
+        default:
+            break;
+        }               
+            
         Position(motor,position_);
     }
     return 0;
